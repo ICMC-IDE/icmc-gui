9186f56b88f317427e92aca47172b9c2dfa6785d
liestrela <estrela@riseup.net>
2026-08-24 23:42:16 -0300

    Set decimal as default

--- src/resources/radix.rs
+++ src/resources/radix.rs
@@ -3,8 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum Radix {
     Binary,
-    Decimal,
     #[default]
+    Decimal,
     Hex,
     Octal,
 }
